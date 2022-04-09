164acdfd35e50f071532517de8630c726836a0fc
charuzain <charuabhizain@gmail.com>
2022-04-08 22:23:16 -0400

    day 16 LHL JS coding challenge

--- challenge16.js
+++ challenge16.js
@@ -1,11 +1,37 @@
-// Your task is to write a function that will take in two position objects.
-//Calculate the average speed from the two positions and return the average speed rounded to the
-// first decimal point.
+/*************************************************************************
+Challenge #16 - 21 Questions
 
-// Each object contains the altitude in meters, and a time stamp in seconds.
+Instructions
+Your task is to write a function that will take in two position objects. Calculate the average speed from the two positions and return the average speed rounded to the first decimal point.
 
-// Input:
+Each object contains the altitude in meters, and a time stamp in seconds.
 
+Examples
+Input:
+
+  const firstPosition = {
+    time: 1637074462,
+    altitude: 1100
+  }
+  const secondPosition = {
+    time: 1637074558,
+    altitude: 2200
+  }
+    
+Output:
+
+  11.5
+	
+
+  *****************************************************************************/
+// ******************************* Solution  ********************************//
+
+
+const getAverageSpeed = (firstPosition, secondPosition) => {
+  const altitueDifference = secondPosition.altitude - firstPosition.altitude;
+  const timeDifference =  secondPosition.time - firstPosition.time;
+  return Math.round(altitueDifference / timeDifference * 10) / 10;
+};
 const firstPosition = {
   time: 1637074462,
   altitude: 1100
@@ -14,16 +40,4 @@ const secondPosition = {
   time: 1637074558,
   altitude: 2200
 };
-
-// Output:
-// 11.5
-
-
-const getAverageSpeed = (firstPosition, secondPosition) => {
-  const altitude = secondPosition.altitude - firstPosition.altitude;
-  const time = secondPosition.time - firstPosition.time;
-  const averageSpeed = Math.round(altitude / time * 10) / 10;
-  return averageSpeed;
-};
-console.log(getAverageSpeed(firstPosition,secondPosition));
-
+console.log(getAverageSpeed(firstPosition, secondPosition));
