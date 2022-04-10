800f1394a3cf6a81c8773aae5b33668ce187d799
charuzain <charuabhizain@gmail.com>
2022-04-09 21:37:25 -0400

    day 17 LHL JS Challenge

--- challenge17.js
+++ challenge17.js
@@ -1,8 +1,49 @@
-// Your task is to write a function that will take in an array of toggle objects and a specific toggle
-//  name.The goal is to switch only the specific toggle, without affecting the other
-//  toggles and then return the updated array.
+/*************************************************************************
+Challenge #17- The moment you’ve all been waiting for...
 
-// Input:
+Instructions
+Your task is to write a function that will take in an array of toggle objects and a specific toggle name. The goal is to switch only the specific toggle, without affecting the other toggles and then return the updated array.
+
+Don't forget that you can use your previous function from the fourth challenge to help you complete it faster!
+Examples
+Input:
+
+const toggleList = [
+  {
+    name: "toggleA",
+    isOn: false
+    },
+  {
+    name: "toggleB",
+    isOn: true
+  }
+]
+const specificToggle = "toggleA"
+    
+Output:
+
+[
+  {
+    name: "toggleA",
+    isOn: true
+  },
+  {
+    name: "toggleB",
+    isOn: true
+  }
+]
+  *****************************************************************************/
+// ******************************* Solution  ********************************//
+
+const switchSpecificToggle = (toggleList, specificToggle) => {
+  return toggleList.map(item =>{
+    if (item.name === specificToggle) {
+      item.isOn = !item.isOn;
+    }
+    return item;
+  });
+  
+};
 const toggleList = [
   {
     name: "toggleA",
@@ -12,33 +53,7 @@ const toggleList = [
     name: "toggleB",
     isOn: true
   }
-]
-const specificToggle = "toggleA"
+];
+const specificToggle = "toggleA";
 
-// Output:
-
-// [
-//   {
-//     name: "toggleA",
-//     isOn: true
-//   },
-//   {
-//     name: "toggleB",
-//     isOn: true
-//   }
-// ]
-
-const switchSpecificToggle = (toggleList, specificToggle) => {
-
-  const newArray = toggleList.map((item)=> 
-  {if(item.name===specificToggle && !item.isOn){
-      item.isOn = true;
-      return item;
-  }
-  
-   else{return item}
-  })
-return newArray
-}
-
-console.log(switchSpecificToggle(toggleList,specificToggle))
+console.log(switchSpecificToggle(toggleList,specificToggle));
